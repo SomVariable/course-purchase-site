b7775806c284d7c98d6a4e166336705d5f86876a
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-01-02 11:38:01 +0200

    added styles for the footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import styles from './Footer.module.css'
-
+import fn from 'classnames'
+import { format } from 'date-fns';
 interface IFooterProp extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>{
   children?: React.ReactNode;
 }
 
 function Footer({className, ...props} : IFooterProp) : JSX.Element {
   return (
-    <div className = {className}>Footer</div>
+    <div className = {fn(styles.footer, className)}>
+      <div className="">
+        OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+      </div>
+      
+      <div className={styles.links}>
+        <a href="#">Пользовательское соглашение</a>
+        <a href="#">Политика конфиденциальности</a>
+      </div>
+      
+    </div>
   )
 }
 
