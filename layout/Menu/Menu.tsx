02ee63c879e993a1d425a10b0fb38e6166fe2319
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-01-08 17:16:33 +0200

    developed firstLevelMenuItems array

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,8 +1,22 @@
 import React, { useContext } from 'react'
+import {BooksIcon} from '../../components';
+import {CoursesIcon} from '../../components';
+import {ProductsIcon} from '../../components';
+import {ServeciesIcon} from '../../components';
 import { AppContext } from '../../context/app.context';
+import { FirstLevelMenuItems } from '../../interfaces/menu.interface';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+
 
 function Menu() {
     const { menu, setMenu, firstCategory } = useContext(AppContext);
+
+    const firstLevelMenuItems : FirstLevelMenuItems[] = [
+        {route: "courses", name: "Courses", icon: <CoursesIcon />, id: TopLevelCategory.Courses},
+        {route: "books", name: "Books", icon: <BooksIcon />, id: TopLevelCategory.Books},
+        {route: "services", name: "Services", icon: <ServeciesIcon />, id: TopLevelCategory.Services},
+        {route: "products", name: "Products", icon: <ProductsIcon />, id: TopLevelCategory.Products}
+    ]
 
     return (
         <div>
@@ -14,3 +28,8 @@ function Menu() {
 }
 
 export default Menu
+
+
+// 2) Menu: create objects menu: [
+//     {route, name, icon, id} * 4
+//    ]
