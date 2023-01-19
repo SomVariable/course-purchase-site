cff94a0083495c07b21036a4fa79c6fddc497aeb
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-01-19 12:09:15 +0200

    updated component Sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
 import React, { DetailedHTMLProps, HTMLAttributes } from 'react'
+import LogoIcon from '../../components/UI/Logo/Logo'
 import Menu from '../Menu/Menu'
 import styles from './Sidebar.module.css'
+import cn from 'classnames'
 
 export interface SidebarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
 
 function Sidebar({className, ...props} : SidebarProps) : JSX.Element {
   return (
-    <div className = {className} {...props}>
+    <div className = {cn(styles.sidebar, className)} {...props}>
+      <LogoIcon />
+      <div className="">search</div>
       <Menu />
     </div>
   )
