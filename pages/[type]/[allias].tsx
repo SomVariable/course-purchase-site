b82b8cb8bd3a437335fe15f87300ad1a0667f754
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-01-19 12:16:43 +0200

    renamed dir course directory to [types]

---
+++ pages/[type]/[allias].tsx
@@ -0,0 +1,79 @@
+import { withLayout } from "../../layout/Layout";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import axios from "axios"
+import { MenuItem } from "../../interfaces/menu.interface";
+import { TopLevelCategory, TopPageModel } from "../../interfaces/page.interface";
+import { ProductModel } from "../../interfaces/product.interface";
+import { ParsedUrlQuery } from "querystring";
+import {IAppContext} from "../../context/app.context"
+import { firstLevelMenuItems } from "../../helpers/helpers";
+
+let firstCategory = TopLevelCategory.Courses
+
+function Course({menu, page, products} : CourseProps): JSX.Element {
+	return (
+		<div>
+            {products && products.length}
+            {!products&& <p>none</p>}
+		</div>
+	);
+}
+
+
+export default withLayout(Course);
+
+export const getStaticPaths: GetStaticPaths = async() => {
+    let paths: string[] = [];
+	for (const m of firstLevelMenuItems) {
+        firstCategory = m.id;
+		const {data: menu} = await axios.get<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/${firstCategory}`);
+		paths = paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
+	}
+	return {
+		paths,
+		fallback: true
+	};
+}
+
+export const getStaticProps: GetStaticProps<CourseProps> = async ({params}: GetStaticPropsContext<ParsedUrlQuery>) => {
+    if(!params){
+        return {
+            notFound: true
+        }
+    }
+    const firstCategoryItem = firstLevelMenuItems.find(m => m.route === params.type)
+    if (!firstCategoryItem)  {
+        return {
+            notFound: true
+        }
+    } 
+
+    try {
+        const page:TopPageModel  = await (await axios.get<TopPageModel[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/byAlias?alias=${params.allias}`)).data[0];
+        const products = await (await axios.get<ProductModel[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/products`)).data.filter(product => product.categories.includes(page.category));
+        firstCategory = firstCategoryItem.id
+        const {data: menu} = await axios.get<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/${firstCategory}`);
+        
+        return {
+            props: {
+                menu,
+                firstCategory,
+                page,
+                products
+            }
+        }        
+    } catch {
+        return {
+            notFound: true
+        }
+    }
+	
+
+}
+
+interface CourseProps extends Record<string, unknown>{
+    menu: MenuItem[];
+	firstCategory: TopLevelCategory;
+	page: TopPageModel;
+	products: ProductModel[];
+}
---
+++ pages/[type]/index.tsx
@@ -0,0 +1,57 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import React from 'react';
+import axios from 'axios';
+import { MenuItem } from '../../interfaces/menu.interface';
+import { withLayout } from '../../layout/Layout';
+import { firstLevelMenuItems } from '../../helpers/helpers';
+import { ParsedUrlQuery } from 'node:querystring';
+import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
+import { ProductModel } from '../../interfaces/product.interface';
+
+let firstCategory = TopLevelCategory.Courses
+
+function Type({ firstCategory }: TypeProps): JSX.Element {
+
+	return (
+		<>
+			Type: {firstCategory}
+		</>
+	);
+}
+
+export default withLayout(Type);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	return {
+		paths: firstLevelMenuItems.map(m => '/' + m.route),
+		fallback: true
+	};
+};
+
+export const getStaticProps: GetStaticProps<TypeProps> = async ({params}: GetStaticPropsContext<ParsedUrlQuery>) => {
+    if (!params) {
+		return {
+			notFound: true
+		};
+	}
+	const firstCategoryItem = firstLevelMenuItems.find(m => m.route == params.type);
+	if (!firstCategoryItem) {
+		return {
+			notFound: true
+		};
+	}
+    firstCategory = firstCategoryItem.id
+    const {data: menu} = await axios.get<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/${firstCategory}`);
+    
+    return {
+        props: {
+            menu,
+            firstCategory: firstCategoryItem.id
+        }
+    }        
+}
+
+interface TypeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: TopLevelCategory;
+}
